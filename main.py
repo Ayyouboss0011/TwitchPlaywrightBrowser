03a5ca8da48f030bbe207622fcd973059827db26
Ayyouboss0011 <ayyoub.ag@gmail.com>
2024-06-28 10:30:15 +0200

    Browser läuft jetzt unendlich

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from playwright.sync_api import sync_playwright
 from playwright_stealth import stealth_sync
-import time
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the path to the extension relative to the script's directory
-path_to_extension = os.path.join(script_dir, "Chrome_Profile", "Extensions", "edibdbjcniadpccecjdfdjjppcpchdlm", "1.1.4_0")
+path_to_extension = os.path.join(script_dir, "Chrome_Profile", "Extensions", "edibdbjcniadpccecjdfdjjppcpchdlm",
+                                 "1.1.4_0")
 user_data_dir = os.path.join(script_dir, "Chrome_Profile")
 
-with sync_playwright() as p:
-    print("Playwright wurde gestartet...")
-    browser = p.chromium.launch_persistent_context(
+
+def keep_browser_alive():
+    with sync_playwright() as p:
+        print("Playwright wurde gestartet...")
+        browser = p.chromium.launch_persistent_context(
             user_data_dir,
             channel="chrome",
             headless=False,
@@ -21,10 +23,18 @@ with sync_playwright() as p:
                 "--autoplay-policy=no-user-gesture-required"
             ],
         )
-    page = browser.new_page()
-    stealth_sync(page)
-    print("Öffne Webseite...")
-    page.goto('https://youtube.com')
-    print("Seite wurde erfolgreich geöffnet.")
+        page = browser.new_page()
+        stealth_sync(page)
+        print("Öffne Webseite...")
+        page.goto('https://www.browserscan.net/bot-detection')
+        print("Seite wurde erfolgreich geöffnet.")
 
-    time.sleep(100000)
+        try:
+            while True:
+                pass  # Keeps the browser session alive indefinitely
+        except KeyboardInterrupt:
+            print("Browser session ended by user.")
+
+
+if __name__ == "__main__":
+    keep_browser_alive()
